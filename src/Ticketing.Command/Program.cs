155f94e55c5b6a2948333c080f9e4d6f2ce4d050
Jorge Medina <medidrones@gmail.com>
2025-11-08 00:39:30 -0300

    Feat: Microservices y el nuevo Swagger

--- src/Ticketing.Command/Program.cs
+++ src/Ticketing.Command/Program.cs
@@ -1,3 +1,4 @@
+using Scalar.AspNetCore;
 using Ticketing.Command.Application;
 using Ticketing.Command.Features.Apis;
 using Ticketing.Command.Infrastructure;
@@ -14,6 +15,13 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference(opt =>
+    {
+        opt.Title = "Microservice Command con Scalar";
+        opt.DarkMode = true;
+        opt.Theme = ScalarTheme.BluePlanet;
+        opt.DefaultHttpClient = new(ScalarTarget.Http, ScalarClient.Http11);
+    });
 }
 
 app.MapMinimalApiEndpoints();
